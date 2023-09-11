024556a0f4afa82ac92b8ef575ee438820eeba5a
Ryan <ryan.stutz06@gmail.com>
2023-09-11 15:46:35 -0400

    Grid is now mutable through use of the size button. Made it so each box is filled in with a different color

--- script.js
+++ script.js
@@ -1,44 +1,62 @@
 let panel = document.getElementById("panel");
 let sizeButton = document.getElementById("size");
-let size = 16;
+let boxes = document.getElementsByClassName("box");
+let defaultSize = 16;
 
 let isDrawing = false;
 
+createGrid(defaultSize);
 
-let row;
-let box;
+function createGrid(gridSize) {
+    
+    let row;
+    let box;
 
-for(let i = 0; i < 16; i++) {
-    row = document.createElement("div");
-    row.classList.add("row");
-    for(let j = 0; j < 16; j++) {
-        box = document.createElement("div");
-        box.classList.add("box");
-        row.appendChild(box);
+    panel.innerText = ""
+
+    for(let i = 0; i < gridSize; i++) {
+        row = document.createElement("div");
+        row.classList.add("row");
+        for(let j = 0; j < gridSize; j++) {
+            box = document.createElement("div");
+            box.classList.add("box");
+            row.appendChild(box);
+        }
+        panel.appendChild(row);
     }
-    panel.appendChild(row);
+
+    addGridEvents();
+
 }
 
-let boxes = document.getElementsByClassName("box");
-
-for(let i = 0; i < boxes.length; i++){
+function addGridEvents () {
     
-    boxes[i].addEventListener("mousedown", (event) => {
-        event.target.style['background-color'] = "black";
-        isDrawing = true;
-    })
-    
-    boxes[i].addEventListener("mousemove", (event) => {
-        if(isDrawing) {
-            event.target.style['background-color']  = "black"; 
-        }
-    })
 
-    boxes[i].addEventListener("mouseup", (event) => {
-        isDrawing = false;
-    })
+    for(let i = 0; i < boxes.length; i++){
+        
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+
+        boxes[i].addEventListener("mousedown", (event) => {
+            event.target.style['background-color'] = `rgb(${r},${g},${b})`;
+            isDrawing = true;
+        })
+        
+        boxes[i].addEventListener("mousemove", (event) => {
+            if(isDrawing) {
+                event.target.style['background-color'] = `rgb(${r},${g},${b})`;
+            }
+        })
+    
+        boxes[i].addEventListener("mouseup", (event) => {
+            isDrawing = false;
+        })
+    }
 }
 
 sizeButton.addEventListener("click", () => {
-    prompt("")
+    createGrid(parseInt(prompt("Enter the size of the grid")));
+    
+
 })
